48b4f67c13a31ae928499f45e7bb272f9ce64e48
Claire <63174119+clairemanthey@users.noreply.github.com>
2022-09-13 09:59:20 -0500

    Updated searchedPosts

--- src/components/searchPosts.js
+++ src/components/searchPosts.js
@@ -1,149 +1,71 @@
-import React, { useState } from "react"
-import { Link } from "gatsby"
-import styled from "styled-components"
-import { useFlexSearch } from "react-use-flexsearch"
-import * as queryString from "query-string"
+import React from "react"
+import { Link, graphql } from "gatsby"
 import "../app.css"
-import { rhythm } from "../utils/typography"
 
-const SearchBar = styled.div`
-  display: flex;
-  border: 1px solid #dfe1e5;
-  border-radius: 10px;
-  margin: 0 150px;
-  height: 3rem;
-  background: #fdfdfd;
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Button from "../components/button"
+import SearchPosts from "../components/searchPosts"
+import Header from "../components/header"
 
-  svg {
-    margin: auto 1rem;
-    height: 20px;
-    width: 20px;
-    color: #9aa0a6;
-    fill: #9aa0a6;
-  }
 
-  input {
-    display: flex;
-    flex: 100%;
-    height: 100%;
-    width: auto;
-    font-family: system-ui, -apple-system, "Fira Sans", sans-serif;
-    font-size: 16px;
-    background-color: transparent;
-    border: none;
-    margin: 0;
-    padding: 0;
-    padding-right: 0.5rem;
-    color: rgb(55, 53, 47);
-    word-wrap: break-word;
-    outline: none;
-  }
-`
+class Blog extends React.Component {
+  render() {
+    const { data, navigate, location } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMdx.edges
+    const localSearchBlog = data.localSearchBlog
 
-const SearchedPosts = ({ results }) =>
-  results.length > 0 ? (
-    results.map(node => {
-      const date = node.date
-      const title = node.title || node.slug
-      const description = node.description
-      const excerpt = node.excerpt
-      const slug = node.slug
+    return (
+      <div className="main">
+        <Header />
 
-      return (
-        <div key={slug} className="container__posts">
-          <h3
-            style={{
-              marginBottom: rhythm(1 / 4),
-            }}
-          >
-            <Link style={{ boxShadow: `none` }} to={`/blog${slug}`}>
-              {title}
-            </Link>
-          </h3>
-          <small>{date}</small>
-          <p
-            dangerouslySetInnerHTML={{
-              __html: description || excerpt,
-            }}
+        <Layout location={this.props.location} title={siteTitle}>
+          <SEO title="All posts" />
+          <Bio />
+          <SearchPosts
+            posts={posts}
+            localSearchBlog={localSearchBlog}
+            navigate={navigate}
+            location={location}
           />
-        </div>
-      )
-    })
-  ) : (
-    <p style={{ textAlign: "center" }}>
-      Sorry, couldn't find any posts matching this search.
-    </p>
-  )
-
-/*Post containter */
-const AllPosts = ({ posts }) => (
-  <div style={{ margin: "20px 0 40px"}} className="container__posts">
-    {posts.map(({ node }) => {
-      const title = node.frontmatter.title || node.fields.slug
-      return (
-        <div key={node.fields.slug}>
-          <h3
-            style={{
-              marginBottom: rhythm(1 / 4),
-              fontFamily: `'Fira Sans Extra Condensed', sans-serif`,
-            }}
-          >
-            <Link style={{ boxShadow: `none` }} to={`/blog${node.fields.slug}`}>
-              {title}
-            </Link>
-          </h3>
-          <small>{node.frontmatter.date}</small>
-          <p
-            dangerouslySetInnerHTML={{
-              __html: node.frontmatter.description || node.excerpt,
-            }}
-            style={{
-              fontFamily: `'Hahmlet', serif`,
-              overflow: `Hidden`
-            }}
-          />
-        </div>
-      )
-    })}
-  </div>
-)
-
-const SearchPosts = ({ posts, localSearchBlog, location, navigate }) => {
-  const { search } = queryString.parse(location.search)
-  const [query, setQuery] = useState(search || "")
-
-  const results = useFlexSearch(
-    query,
-    localSearchBlog.index,
-    JSON.parse(localSearchBlog.store)
-  )
-
-  return (
-    <>
-      <SearchBar>
-        <svg
-          focusable="false"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-        </svg>
-        <input
-          id="search"
-          type="search"
-          placeholder="Search all posts"
-          value={query}
-          onChange={e => {
-            navigate(
-              e.target.value ? `/blog/?search=${e.target.value}` : "/blog/"
-            )
-            setQuery(e.target.value)
-          }}
-        />
-      </SearchBar>
-      {query ? <SearchedPosts results={results} /> : <AllPosts posts={posts} />}
-    </>
-  )
+          <Link to="/">
+            <Button>Go Home</Button>
+          </Link>
+        </Layout>
+      </div>
+    )
+  }
 }
 
-export default SearchPosts
+export default Blog
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    localSearchBlog {
+      index
+      store
+    }
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
