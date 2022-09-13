73a280ee66d6a4e6a2ecc4dda21a769a52fb329d
Claire <63174119+clairemanthey@users.noreply.github.com>
2022-09-13 09:58:53 -0500

    Updated index.js
    Updated index.js with new classnames

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,18 +20,20 @@ class IndexPage extends React.Component {
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <h1 className="header-one">
-          Hey people{" "}
-          <span role="img" aria-label="wave emoji">
-            👋
-          </span>
-        </h1>
-        <p className="para para-home">Welcome to my blog. Follow me along my journey to making the web a more beautiful place.</p>
-        <p className="para para-home">Starting from the basics with HTML and working up to add functionality with JS and making it look
-        good with CSS.</p>
-        <Link to="/blog/">
-          <Button marginTop="35px">Go to Blog</Button>
-        </Link>
+        <section className="main-section">
+          <h1 className="header-one">
+            Hey people{" "}
+            <span role="img" aria-label="wave emoji">
+              👋
+            </span>
+          </h1>
+          <p className="para blog-para para-home">Welcome to my blog. Follow me along my journey to making the web a more beautiful place.</p>
+          <p className="para blog-para para-home">Starting from the basics with HTML and working up to add functionality with JS and making it look
+          good with CSS.</p>
+          <Link to="/blog/">
+            <Button marginTop="35px">Go to Blog</Button>
+          </Link>
+        </section>
       </Layout>
       </main>
     )
